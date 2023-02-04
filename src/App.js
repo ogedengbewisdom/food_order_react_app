4cd1a9fa6325d143c92d9897c91f65734750732a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-04 04:43:39 +0100

    rendering meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meal";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
