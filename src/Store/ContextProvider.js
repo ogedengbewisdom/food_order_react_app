48141918f3cfa75bdc32c64de4d13e1cbad17bab
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-06 03:54:15 +0100

    contextProvider with th value

---
+++ src/Store/ContextProvider.js
@@ -0,0 +1,24 @@
+import CartContext from "./auth-context"
+
+
+const ContextProvider = (props) => {
+
+    const removeItemHandler = (id) => {}
+
+    const addItemHandler = (item) => {}
+
+    const contextValue = {
+        item: [],
+        totalAmount: 0,
+        removeItem: removeItemHandler,
+        addItem: addItemHandler
+    } 
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default ContextProvider
