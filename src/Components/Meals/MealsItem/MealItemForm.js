28a9c5fa4f6b9ec94eef49c549e539b1d3c756a4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-06 19:09:24 +0100

    working with refs and forward refs

--- src/Components/Meals/MealsItem/MealItemForm.js
+++ src/Components/Meals/MealsItem/MealItemForm.js
@@ -1,10 +1,28 @@
+import { useRef, useState } from "react"
 import Input from "../../UI/Input"
 import classes from "./MealItemForm.module.css"
 
 const MealItemForm = (props) => {
+
+    const amountInputRef = useRef()
+
+    const [amountIsValid, setAmountIsValid] = useState(true)
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value
+        const enteredAmountNumber = +enteredAmount
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false)
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+    }
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+            ref={amountInputRef}
              label="Amount"
              input = {{
                 id: "amount" + props.id,
@@ -12,10 +30,11 @@ const MealItemForm = (props) => {
                 max: "5",
                 min: "1",
                 step: "1",
-                defaultValue: "0"
+                defaultValue: "1"
              }}
             />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please Enter a Valid Amount From 1 - 5</p>}
         </form>
     )
 }
