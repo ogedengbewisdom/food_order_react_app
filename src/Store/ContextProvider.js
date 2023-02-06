be94613bc6f046f38100ddc409b78b1ef9ab3930
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-06 05:22:22 +0100

    Adding a Cart Reducer

--- src/Store/ContextProvider.js
+++ src/Store/ContextProvider.js
@@ -1,15 +1,37 @@
+import { useReducer } from "react"
 import CartContext from "./auth-context"
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === "ADD") {
+        const updatedItem = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return {
+            items: updatedItem,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+    return defaultCartState
+}
 
 const ContextProvider = (props) => {
 
+    const [cartState, dispatchCartState] = useReducer(cartReducer, defaultCartState)
+
     const removeItemHandler = (id) => {}
 
-    const addItemHandler = (item) => {}
+    const addItemHandler = (item) => {
+        dispatchCartState({type: "ADD", item: item})
+    }
 
     const contextValue = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         removeItem: removeItemHandler,
         addItem: addItemHandler
     } 
