26ed90c93f79f8fc75e39f1b3c75c113545c32f5
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-10 12:11:53 +0100

    shopping cart and mapping Cartitems component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,8 @@ import { useContext } from "react"
 import CartContext from "../../Store/auth-context"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
+import CartItems from "./CartItems"
+
 
 
 const Cart = (props) => {
@@ -9,7 +11,19 @@ const Cart = (props) => {
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
 
-    const cartItems = <ul className={classes["cart-items"]}>{cartCtx.items.map(item => <li key={Math.random()}>{item.name}</li>)}</ul>
+    const addItemHandler = item => {}
+    const removeItemHandler = id => {}
+
+    const cartItems = <ul className={classes["cart-items"]}>{cartCtx.items.map(item =>
+         <CartItems
+          key={item.id} 
+          name={item.name} 
+          amount={item.amount} 
+          price={item.price} 
+          onRemove={removeItemHandler} 
+          onAdd={addItemHandler}
+           />)}
+           </ul>
     const showOrderBtn = cartCtx.items.length > 0
     return (
         <Modal onClick={props.onCloseCart}>
