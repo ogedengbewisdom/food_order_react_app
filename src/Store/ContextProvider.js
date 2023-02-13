f3c6b70f4c6a06e80ae42115b14f39403515f488
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-13 23:40:32 +0100

    making item removable

--- src/Store/ContextProvider.js
+++ src/Store/ContextProvider.js
@@ -8,18 +8,18 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === "ADD") {
-        
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
         const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
         const existingCartItem = state.items[existingCartItemIndex]
+
         let updatedItems;
-        
+
         if ( existingCartItem ) {
-           const updatedItem = {
+            const updatedItem = {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount
-            }
-
+            } 
             updatedItems = [...state.items]
             updatedItems[existingCartItemIndex] = updatedItem
         } else {
@@ -30,18 +30,23 @@ const cartReducer = (state, action) => {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         }
-    } if (action.type === "REMOVE") {
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id)
+    } 
+
+    if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id) 
         const existingCartItem = state.items[existingCartItemIndex]
         const updatedTotalAmount = state.totalAmount - existingCartItem.price
-        let updatedItems
+
+        let updatedItems;
+
         if (existingCartItem.amount === 1) {
             updatedItems = state.items.filter(item => item.id !== action.id)
         } else {
             const updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1}
             updatedItems = [...state.items]
             updatedItems[existingCartItemIndex] = updatedItem
-        } 
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
