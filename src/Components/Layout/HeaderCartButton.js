0b64a1d93d9f3766de6c55a3bae2210e9ffe56b3
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-14 00:39:44 +0100

    useEffect and useState react hook

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartContext from "../../Store/auth-context"
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
@@ -7,11 +7,28 @@ import classes from "./HeaderCartButton.module.css"
 const HeaderCartButton = (props) => {
 
     const cartCtx = useContext(CartContext)
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const {items} = cartCtx
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount
     }, 0)
 
-    const keyframeButtons  = `${classes.button} ${classes.bump}`
+    const [btnIsHighligted, setBtnIsHighLighted] = useState(false)
+
+    const keyframeButtons  = `${classes.button} ${btnIsHighligted ? classes.bump : ""}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighLighted(true)
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false)
+        }, 300);
+        
+        return () => {
+            clearTimeout(timer)
+        }
+    },[items])
     return (
         <button className={keyframeButtons} onClick={props.onClick}>
             <span className={classes.icon}>
