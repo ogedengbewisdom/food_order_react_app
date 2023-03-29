1a3363ea4d216e1a9ea8f7bdc8bce4193129e603
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-29 23:02:02 +0100

    Rendering the checkout component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,13 +1,14 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import CartContext from "../../Store/auth-context"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
 import CartItems from "./CartItems"
+import Checkout from "./Checkout"
 
 
 
 const Cart = (props) => {
-
+    const [isCheckout, setIsCheckout] = useState(false)
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
@@ -18,6 +19,10 @@ const Cart = (props) => {
     }
     const removeItemHandler = id => {
         cartCtx.removeItem(id);
+    }
+
+    const orderButtonHandler = () => {
+        setIsCheckout(true)
     }
 
     const cartItems = <ul className={classes["cart-items"]}>{cartCtx.items.map(item =>
@@ -31,6 +36,12 @@ const Cart = (props) => {
            />)}
            </ul>
     const showOrderBtn = cartCtx.items.length > 0
+
+    const modalAction = (
+    <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>Close</button>
+        {showOrderBtn && <button className={classes.button} onClick={orderButtonHandler}>Order</button>}
+  </div>)
     return (
         <Modal onClick={props.onCloseCart}>
             {cartItems}
@@ -38,10 +49,8 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes["button--alt"]} onClick={props.onCloseCart}>Close</button>
-                {showOrderBtn && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onCloseCart} />}
+            {!isCheckout && modalAction}
         </Modal>
     )
 }
