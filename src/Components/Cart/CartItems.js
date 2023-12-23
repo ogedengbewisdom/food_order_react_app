6a85cbca944dc47cf4769ef5dfa062421076cf0c
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-12-23 14:18:21 +0100

    meals item

--- src/Components/Cart/CartItems.js
+++ src/Components/Cart/CartItems.js
@@ -1,7 +1,7 @@
 import classes from "./CartItems.module.css"
 
 const CartItems = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+    const price = `N${props.price.toFixed(2)}`
     return (
         <li className={classes["cart-item"]}>
             <div>
